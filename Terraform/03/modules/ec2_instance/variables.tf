38f19c0dcfb779fab969ee8a69a4979553f25179
Infravyom IT Technologies <pradeep@infravyom.com>
2025-10-23 15:14:56 +0530

    Update variables.tf

--- Terraform/03/modules/ec2_instance/variables.tf
+++ Terraform/03/modules/ec2_instance/variables.tf
@@ -1,11 +1,15 @@
 variable "ami_value" {
-    description = "value for the ami"
+  description = "The AMI ID to use for the EC2 instance"
+  type        = string
 }
 
 variable "instance_type_value" {
-    description = "value for instance_type"
+  description = "The type of instance to create"
+  type        = string
+  default     = "t2.micro"
 }
 
 variable "subnet_id_value" {
-    description = "value for the subnet_id"
+  description = "The Subnet ID where the instance will be launched"
+  type        = string
 }
